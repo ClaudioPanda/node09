12fee1953431bca4c529f9e9ede428c514310d12
BigPandita <lic.claudio.landa@gmail.com>
2022-05-24 07:26:18 -0400

    add escritorio socket listener

--- sockets/controller.js
+++ sockets/controller.js
@@ -1,23 +1,58 @@
 const TicketControl = require('../models/ticket_control.js');
 
-const ticketControl= new TicketControl();
+const ticketControl = new TicketControl();
 
 const socketController = (socket) => {
-      
-    socket.on('enviar-mensaje', ( payload, callback ) => {
-        
+
+
+
+    socket.emit('ultimo-ticket', ticketControl.ultimo);
+
+
+
+    socket.on('siguiente-ticket', (payload, callback) => {
+
+
+        const siguiente = ticketControl.siguiente();
+        callback(siguiente);
+
+        // TODO: Notificar que hay un nuevo ticket pendiente
+
+        /*         
         const id = 123456789;
         callback( id );
 
-        socket.broadcast.emit('enviar-mensaje', payload );
+        socket.broadcast.emit('enviar-mensaje', payload ); */
 
     })
 
-}
+    socket.on('atender-ticket', ({ escritorio }, callback) => {
+        if (!escritorio) {
+            return callback({
+                ok: false,
+                msg: 'El escritorio es obligatorio'
+            });
 
+        };
 
+        const ticket = ticketControl.atenderTicket(escritorio);
+
+        if (!ticket) {
+            callback({
+                ok: false,
+                msg: 'No hay tickets'
+            });
+        } else {
+            callback({
+                ok: true,
+                ticket
+            });
+        }
+
+    });
+
+};
 
 module.exports = {
     socketController
 }
-
